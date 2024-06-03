48c62ed59618964eed6cb63a21ea011518229bdf
Endy135 <endinoungang@gmail.com>
2024-06-03 10:52:06 +0000

    change version de postgres

--- script_elt.py
+++ script_elt.py
@@ -18,9 +18,9 @@ def wait_for_postgres(host, max_essai=5, delai_secondes=5):
             print(f"Erreur de connections à postgres{e}")
             essai += 1
             print(
-                f"Nouvelle tentative dans une delai de {delai_secondes} seconds... (Attemp {essai / {max_essai}}")
+                f"Nouvelle tentative dans un delai de {delai_secondes} seconds... (tentative {essai} / {max_essai}")
             time.sleep(delai_secondes)
-    print("Nombre maximum d'essai atteind. Terminé")
+    print("Nombre maximum d'essai atteint. Terminé")
     return False
 
 if not wait_for_postgres(host = "source_postgres"):
@@ -32,7 +32,7 @@ source_config = {
         'dbname' : 'source_db',
         'user': 'postgres',
         'password': 'postgres',
-        'host' : 'destination_db'
+        'host' : 'source_postgres'
     }
 
 destination_config = {
@@ -45,7 +45,7 @@ destination_config = {
 commande_sauvegarde = [
     'pg_dump', 
     '-h', source_config['host'],
-    '-u', source_config['user'],
+    '-U', source_config['user'],
     '-d', source_config['dbname'],
     '-f', 'data_dump.sql',
     '-w'
@@ -56,17 +56,16 @@ subprocess_env = dict(PGPASSWORD = source_config['password'])
 subprocess.run(commande_sauvegarde, env = subprocess_env, check=True)
 
 commande_chargement = [
-    'pg_dump', 
+    'psql', 
     '-h', destination_config['host'],
-    '-u', destination_config['user'],
+    '-U', destination_config['user'],
     '-d', destination_config['dbname'],
-    '-a', '-f', 'data_dump.sql',
-    '-w'
+    '-a', '-f', 'data_dump.sql'
 ]
 
 subprocess_env = dict(PGPASSWORD = destination_config['password'])
 
-subprocess.run(commande_chargement, env=subprocess_env,check=True)
+subprocess.run(commande_chargement, env=subprocess_env, check=True)
 
 
 print("Processus d'ELT terminé")
